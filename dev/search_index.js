73ed13fab8c275595f060033822a89bf858c708e
Documenter.jl <documenter@juliadocs.github.io>
2023-08-30 10:12:53 +0000

    build based on d4c83a6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SWCFluxDeps","category":"page"},{"location":"#SWCFluxDeps","page":"Home","title":"SWCFluxDeps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SWCFluxDeps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SWCFluxDeps]","category":"page"}]
+}
